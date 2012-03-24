bb225793c756ecd542872a74f9a31a710081210a
Charlie DeTar <cfd@media.mit.edu>
2012-03-24 15:27:29 -0400

    bugfixes and more on simple

--- parse_data.py
+++ parse_data.py
@@ -10,30 +10,37 @@ column_maps = {
     'q0007': {
         'question': "How did you find this survey?",
         'type': 'choose_one_plus_other',
+        'widget': 'single_choice',
     },
     'q0008': {
         'question': "Have you ever been to an Occupy camp?",
         'type': 'free',
+        'widget': 'single_choice',
     },
     'q0009': {
         'question': "Please describe how frequently you have been to a camp.  Choose only one answer:",
         'type': 'choose_one',
+        'widget': 'single_choice',
     },
     'q0010': {
         'question': "In your visits to the Occupy camp, you have: (Select all that apply)",
         'type': 'choose_any_plus_other',
+        'widget': 'multi_choice',
     },
     'q0011': {
         'question': "Have you participated in any of the following activities related to the Occupy movement?  Select all that apply.",
         'type': 'choose_any_plus_other',
+        'widget': 'multi_choice',
     },
     'q0012': {
         'question': "Would you consider the Occupy movement to be the first movement you have participated in?",
         'type': 'free',
+        'widget': 'single_choice',
     },
     'q0013': {
         'question': "Do you participate in any of the following",
         'type': 'matrix',
+        'widget': 'matrix',
         'rows': [
             'Political Party',
             'Labor Union',
@@ -53,6 +60,7 @@ column_maps = {
     'q0014': {
         'question': "Here are some different forms of political and social action people can take.  Please indicate, for each one, if you have done this:",
         'type': 'matrix',
+        'widget': 'matrix',
         'rows': [
             'Signed a petition',
             'Boycotted, or deliberately bought, certain products for political, ethical, or environmental reasons',
@@ -67,6 +75,7 @@ column_maps = {
     'q0015': {
         'question': "These are some sources that you might or might not use for news and information about the Occupy movement.  Please indicate whether you used htese sources for news and information about hte Occupy movement.",
         'type': 'matrix',
+        'widget': 'matrix',
         'rows': [
             'Word of mouth',
             'Discussions at Occupy camps or face to face',
@@ -92,71 +101,87 @@ column_maps = {
     'q0016': {
         'question': "If you participate in the Occupy movement, what TOP THREE concerns motivate you TO PARTICIPATE?  Please use single words if possible, and list them in order of importance.",
         'type': 'top_three_words',
+        'widget': 'multi_choice',
     },
     'q0017': {
         'question': "If you do not participate in the Occupy movement, what TOP THREE reasons explain why you HAVE NOT PARTICIPATED?  Please use single words if possible, and list them in order of importance.",
         'type': 'top_three_words',
+        'widget': 'multi_choice',
     },
     'q0018_0001': {
         'question': "What year were you born?",
         'type': 'free',
+        'widget': 'single_choice',
     },
     'q0019': {
         'question': "Your gender (check all that apply)",
         'type': 'choose_any_plus_other',
+        'widget': 'multi_choice',
     },
     'q0020': {
         'question': "Your sexual identity (check all that apply)",
         'type': 'choose_any_plus_other',
+        'widget': 'multi_choice',
     },
     'q0021': {
         'question': "What best describes your employment status during the last month?  (check all that apply)",
         'type': 'choose_any_plus_other',
+        'widget': 'multi_choice',
     },
     'q0022': {
         'question': "What best describes your present housing status?",
         'type': "choose_one_plus_other",
+        'widget': 'single_choice',
     },
     'q0023': {
         'question': "What is your marital status?",
         'type': "choose_one_plus_other",
+        'widget': 'multi_choice',
     },
     'q0024': {
         'question': "How many people do you support with your income?",
         'type': 'multi',
         'rows': ['Number of children under 18?', 'Number of adults you support?'],
+        'widget': 'matrix',
     },
     'q0025': {
         'question': "Do you identify as:",
         'type': 'choose_one_plus_other',
+        'widget': 'single_choice',
     },
     'q0026': {
         'question': "Do you live in the US?  If you are living elsewhere temporarily, select Yes.",
         'type': "yes_no",
+        'widget': 'single_choice',
     },
     'q0028': {
         'question': "How many years of education have you completed?",
         'type': 'free',
+        'widget': 'single_choice',
     },
 
     'q0030': {
         'question': "Describe your race or ethnicity.",
         'type': 'free',
+        'widget': 'single_choice',
     },
     'q0031': {
         'question': "Which political party do you identify with most closely?",
         'type': 'multi',
         'rows': ["I do or don't associate", "Political party"],
+        'widget': 'matrix',
     },
     'q0032': {
          'question': "Voting activity: Did you vote in your most recent nationwide election?",
          'type': 'multi',
          'rows': ["Eligibility", "Voted for"],
+        'widget': 'matrix',
      },
     'q0033': {
          'question': "Do you plan to vote in your next nationwide election?",
          'type': 'multi',
          'rows': ['Intention to vote', "Voting for"],
+         'widget': 'matrix',
      },
 }
 
@@ -178,36 +203,53 @@ def load_data():
                     cleaned_output_rows[i][key] = val
             elif q['type'] in ("choose_one", "choose_any_plus_other", "choose_one_plus_other"):
                 q_cols = [c for c in top if c.startswith(key)]
-                # Throwing out the free-entry "other" column.
-                if q['type'].endswith("_plus_other"):
-                    q_cols.pop()
                 for q_col in q_cols:
                     for i, row in enumerate(data_rows):
                         vset = cleaned_output_rows[i].get(key, set())
-                        val = dict(zip(top, row))[q_col]
-                        if val.lower() not in ("no answer", "not applicable"):
-                            q['choices'][val] += 1
-                            vset.add(val)
+                        val = dict(zip(top, row))[q_col].lower()
+                        if val.lower() in ("no answer", "not applicable", ""):
+                            val = "no answer"
+                        q['choices'][val] += 1
+                        vset.add(val)
                         cleaned_output_rows[i][key] = vset
+
+                # Remove 'other' choices that have only 1 response
+                for val, count in q['choices'].items():
+                    if count == 1:
+                        del q['choices'][val]
+                for row in cleaned_output_rows:
+                    if len(row[key]) > 1:
+                        try:
+                            row[key].remove("no answer")
+                        except KeyError:
+                            pass
+                    for val in list(row[key]):
+                        if val not in q['choices']:
+                            row[key].remove(val)
+
             elif q['type'] == 'top_three_words':
                 q_cols = [key + "_000%i" % i for i in range(1, 4)]
                 for q_col in q_cols:
                     for row in data_rows:
                         val = dict(zip(top, row))[q_col]
                         q['choices'][val.lower()] += 1
+
                 # Throw out 'top three words' with only 1 response.
                 for choice, count in q['choices'].items():
                     if count == 1:
                         del q['choices'][choice]
+
+                # Get cleaned output rows
                 for q_col in q_cols:
                     for i,row in enumerate(data_rows):
                         vset = cleaned_output_rows[i].get(key, set())
-                        val = dict(zip(top, row))[q_col]
+                        val = dict(zip(top, row))[q_col].lower()
                         if val in q['choices']:
                             vset.add(val)
                         else:
                             vset.add("other")
                         cleaned_output_rows[i][key] = vset
+
             elif q['type'] in ('matrix', 'multi'):
                 q_cols = [c for c in top if c.startswith(key)]
                 for col, name in zip(q_cols, q['rows']):
@@ -226,9 +268,9 @@ def load_data():
                     row[q] = list(answer)
 
     with open('questions.json', 'w') as fh:
-        json.dump(column_maps, fh, indent=4)
+        json.dump(column_maps, fh, indent=2)
     with open('data.json', 'w') as fh:
-        json.dump(cleaned_output_rows, fh)
+        json.dump(cleaned_output_rows, fh, indent=2)
 
 if __name__ == "__main__":
     load_data()
--- simplevis.js
+++ simplevis.js
@@ -27,6 +27,24 @@ $.ajax({
     }
 });
 
+function renderChoice(choice, q, count) {
+    var el = $("<span class='choice'></span>");
+    el.append(
+        $("<a href='#'>" + (choice || "(blank)") + "</a>").click(function() {
+            constraints[q] = choice;
+            render();
+            return false;
+        }).addClass(count == 0 ? 'zero' : '')
+    );
+    el.append("<span class='count" + (count == 0 ? ' zero' : '') + "'>(" + count + ")</span>");
+    el.append(" ");
+    return el;
+}
+function sortCopy(array) {
+    var copy = array.slice();
+    copy.sort();
+    return copy;
+}
 
 var constraints = {};
 function render() {
@@ -35,9 +53,8 @@ function render() {
     var total = 0;
     for (var q in questions) {
         counts[q] = {}
-        if (questions[q].type == 'matrix') {
-            continue;
-        } else {
+        if (questions[q].widget == 'matrix') {
+        } else if ($.inArray(questions[q].widget, ['single_choice', 'multi_choice']) != -1) {
             for (var choice in questions[q].choices) {
                 counts[q][choice] = 0;
             }
@@ -47,18 +64,28 @@ function render() {
         var row = data[i];
         var matched = true;
         for (var q in constraints) {
-            if (row[q] != constraints[q]) {
+            if (questions[q].widget == 'single_choice' && row[q] != constraints[q]) {
                 matched = false;
-                break;
+            } else if (questions[q].widget == 'multi_choice' && $.inArray(constraints[q], row[q]) == -1) {
+                matched = false;
             }
+            if (!matched) 
+                break;
         }
         if (matched) {
             total += 1;
             for (var q in counts) {
-               counts[q][row[q]] += 1; 
+                if (questions[q].widget == 'single_choice') {
+                    counts[q][row[q]] += 1; 
+                } else if (questions[q].widget == 'multi_choice') {
+                    for (var j = 0; j < row[q].length; j++) {
+                        counts[q][row[q][j]] += 1
+                    }
+                } 
             }
         }
     }
+    console.log(counts);
 
     // Render facets
     $("#total").html(total);
@@ -68,25 +95,15 @@ function render() {
         qdiv.append("<h2>" + questions[q].question + "</h2>");
         adiv = $("<div class='answers'></div>");
         qdiv.append(adiv);
-        if (questions[q].type == 'matrix') {
-        } else {
+        if (questions[q].widget == 'matrix') {
+        } else if ($.inArray(questions[q].widget, ['single_choice', 'multi_choice']) != -1) {
             var sortedChoices = [];
             for (var choice in questions[q].choices) {
                 sortedChoices.push(choice);
             }
             sortedChoices.sort();
             for (var i = 0; i < sortedChoices.length; i++) {
-                (function(choice, q) {
-                    var count = counts[q][choice];
-                    adiv.append(
-                        $("<a href='#'>" + (choice || "(blank)") + "</a>").click(function() {
-                            constraints[q] = choice;
-                            render();
-                            return false;
-                        }).addClass(count == 0 ? 'zero' : '')
-                    );
-                    adiv.append("<span class='count" + (count == 0 ? ' zero' : '') + "'>(" + count + ")</span>");
-                })(sortedChoices[i], q);
+                adiv.append(renderChoice(sortedChoices[i], q, counts[q][sortedChoices[i]]));
             }
         }
         $("#questions").append(qdiv);
